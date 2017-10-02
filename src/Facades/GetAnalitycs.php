a577ced218e873356b6abf0befa05820b9e196b2
nam-co <mm@maksuco.com>
2017-10-02 12:58:15 -0500

    Create GetAnalitycs.php

---
+++ src/Facades/GetAnalitycs.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Maksuco\GetAnalitycs\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class GetAnalitycs extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return ('maksuco-getanalitycs');
+    }
+}
