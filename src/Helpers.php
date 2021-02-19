a71904405355b92a4b0c1185863f471fb08bfca9
nam-co <mm@maksuco.com>
2021-02-19 09:58:11 -0500

    up

--- src/Helpers.php
+++ src/Helpers.php
@@ -606,26 +606,6 @@ function COUNTRY_CONTINENTS($countryCode) {
 		return "Good night";
 	}
 
-	
-
-	function generateGreetings(){
-
-		var currentHour = moment().format("HH");
-	
-		if (currentHour >= 3 && currentHour < 12){
-				return "Good Morning";
-		} else if (currentHour >= 12 && currentHour < 15){
-				return "Good Afternoon";
-		}   else if (currentHour >= 15 && currentHour < 20){
-				return "Good Evening";
-		} else if (currentHour >= 20 && currentHour < 3){
-				return "Good Night";
-		} else {
-				return "Hello"
-		}
-	
-	}
-
 
 
 
