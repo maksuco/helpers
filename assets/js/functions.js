5699796d21a78955fc6fe518e4b6ddbd39c26b5c
nam-co <mm@maksuco.com>
2021-08-22 23:36:39 -0500

    up

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,16 @@
+//PACKAGES
+const Toast = Swal.mixin({
+	toast: true,
+	position: 'top-end',
+	showConfirmButton: false,
+	timer: 4000,
+	timerProgressBar: true,
+	didOpen: (toast) => {
+	  toast.addEventListener('mouseenter', Swal.stopTimer)
+	  toast.addEventListener('mouseleave', Swal.resumeTimer)
+	}
+})
+
 //GENERAL
 var lang = lang ?? 'en';
 moment.locale(lang);
