84fac57f53dbc0ad9e19769d4abe2a277a622c04
nam-co <mm@maksuco.com>
2022-09-16 15:09:12 -0500

    colors

--- src/Traits/Colors.php
+++ src/Traits/Colors.php
@@ -1,220 +1,74 @@
 <?php
 namespace Maksuco\Helpers\Traits;
-use Mexitek\PHPColors\Color;
-use Exception;
+use OzdemirBurak\Iris\Color\Hex;
+use OzdemirBurak\Iris\Color\Hexa;
 
 trait Colors {
 
 
-    public function gradient($originalColor,$amount=17) {
-        $color = new Color($originalColor);
-        $colors[0] = $originalColor;
+    public function gradientCSS($firstColor,$secondColor=false,$angle=160,$percentage=20,$opacity=0,$amount=15) {
+        $firstColor = $this->checkHEX($firstColor); 
+        if($firstColor==false){ return false; };
+        if($secondColor==false){
+            $secondColor = $this->spinColor($firstColor,30,$amount)->fadeOut($opacity);
+        }
+        return 'linear-gradient('.$angle.'deg, '.$firstColor.' '.$percentage.'%, '.$secondColor.' 100%)';
+    }
+
+    public function gradientCSS2($color,$color2=false,$opacity=0) {
+        $color = $this->checkHEX($color); 
+        if($color==false){ return false; };
+        $secondColor = $this->spinColor($color,30,1)->fadeOut($opacity);
+        if($color2) {
+            $temp_radial1 = new Hexa($color2);
+            $radial1_dist = 100;
+        } else {
+            $temp_radial1 = new Hexa($color);
+            $radial1_dist = 150;
+        }
+        $radial1 = $temp_radial1->spin(180)->fade(50);
+
+        //$radial1 = $this->spinColor($color,30,70);
+        $radial2 = $this->spinColor($color,30,30)->lighten(70)->fade(50);
+        // $radial1Out = $radial1->fade(0);
+        // $radial2Out = $radial2->fade(0);
+        return <<<HTML
+            radial-gradient(circle at $radial1_dist% 65%, $radial1 0%, transparent 70%), 
+            radial-gradient(circle at -40% 30%, $radial2 0%, transparent 80%), 
+            linear-gradient(180deg, $color 0%, $secondColor 99.72%);
+        HTML;
+    }
+
+    public function spinColor($color,$value=30,$change=0) {
+        $color = new Hexa($color);
         if($color->isLight()){
-            $colors[1] = '#'.$color->darken($amount);
-        } else {
-            $colors[1] = '#'.$color->lighten($amount);
+            return $color->darken($change)->spin($value*.5);
         }
-        $colors['css'] = 'linear-gradient(180deg, rgba('.$colors[0].',1) 0%, rgba('.$colors[1].',1) 100%);';
-        return $colors;
+        return $color->lighten($change)->spin($value);
     }
 
+    public function mixColor($firstColor,$secondColor,$value=50) {
+        $firstColor = $this->checkHEX($firstColor); 
+        if($firstColor==false){ return false; };
+        $color = new Hex($firstColor);
+        return $color->mix(new Hex($secondColor), $value);
+    }
 
-    private static function sanitizeHex(string $hex): string
-    {
-        // Strip # sign if it is present
-        $color = str_replace("#", "", $hex);
+    public function alphaColor($color,$opacity=0) {
+        $color = $this->checkHEX($color); 
+        if($color==false){ return false; };
+        $color = new Hex($color);
+        return $color->fadeIn($opacity);
+    }
 
-        // Validate hex string
-        if (!preg_match('/^[a-fA-F0-9]+$/', $color)) {
-            throw new Exception("HEX color does not match format");
+    public function checkHEX($color) {
+        $color = str_replace("#", "", $color);
+        if(ctype_xdigit($color) && strlen($color)>5){
+            return str_starts_with($color, '#')? $color : '#'.$color;
         }
-
-        // Make sure it's 6 digits
-        if (strlen($color) === 3) {
-            $color = $color[0] . $color[0] . $color[1] . $color[1] . $color[2] . $color[2];
-        } elseif (strlen($color) !== 6) {
-            throw new Exception("HEX color needs to be 6 or 3 digits long");
-        }
-
-        return $color;
-    }
-
-    public static function hexToHsl(string $color): array
-    {
-        // Sanity check
-        $color = self::sanitizeHex($color);
-
-        // Convert HEX to DEC
-        $R = hexdec($color[0] . $color[1]);
-        $G = hexdec($color[2] . $color[3]);
-        $B = hexdec($color[4] . $color[5]);
-
-        $HSL = array();
-
-        $var_R = ($R / 255);
-        $var_G = ($G / 255);
-        $var_B = ($B / 255);
-
-        $var_Min = min($var_R, $var_G, $var_B);
-        $var_Max = max($var_R, $var_G, $var_B);
-        $del_Max = $var_Max - $var_Min;
-
-        $L = ($var_Max + $var_Min) / 2;
-
-        if ($del_Max == 0) {
-            $H = 0;
-            $S = 0;
-        } else {
-            if ($L < 0.5) {
-                $S = $del_Max / ($var_Max + $var_Min);
-            } else {
-                $S = $del_Max / (2 - $var_Max - $var_Min);
-            }
-
-            $del_R = ((($var_Max - $var_R) / 6) + ($del_Max / 2)) / $del_Max;
-            $del_G = ((($var_Max - $var_G) / 6) + ($del_Max / 2)) / $del_Max;
-            $del_B = ((($var_Max - $var_B) / 6) + ($del_Max / 2)) / $del_Max;
-
-            if ($var_R == $var_Max) {
-                $H = $del_B - $del_G;
-            } elseif ($var_G == $var_Max) {
-                $H = (1 / 3) + $del_R - $del_B;
-            } elseif ($var_B == $var_Max) {
-                $H = (2 / 3) + $del_G - $del_R;
-            }
-
-            if ($H < 0) {
-                $H++;
-            }
-            if ($H > 1) {
-                $H--;
-            }
-        }
-
-        $HSL['H'] = ($H * 360);
-        $HSL['S'] = $S;
-        $HSL['L'] = $L;
-
-        return $HSL;
-    }
-
-    public static function hslToHex(array $hsl = array()): string
-    {
-        // Make sure it's HSL
-        if (empty($hsl) || !isset($hsl["H"], $hsl["S"], $hsl["L"])) {
-            throw new Exception("Param was not an HSL array");
-        }
-
-        list($H, $S, $L) = array($hsl['H'] / 360, $hsl['S'], $hsl['L']);
-
-        if ($S == 0) {
-            $r = $L * 255;
-            $g = $L * 255;
-            $b = $L * 255;
-        } else {
-            if ($L < 0.5) {
-                $var_2 = $L * (1 + $S);
-            } else {
-                $var_2 = ($L + $S) - ($S * $L);
-            }
-
-            $var_1 = 2 * $L - $var_2;
-
-            $r = 255 * self::hueToRgb($var_1, $var_2, $H + (1 / 3));
-            $g = 255 * self::hueToRgb($var_1, $var_2, $H);
-            $b = 255 * self::hueToRgb($var_1, $var_2, $H - (1 / 3));
-        }
-
-        // Convert to hex
-        $r = dechex(round($r));
-        $g = dechex(round($g));
-        $b = dechex(round($b));
-
-        // Make sure we get 2 digits for decimals
-        $r = (strlen("" . $r) === 1) ? "0" . $r : $r;
-        $g = (strlen("" . $g) === 1) ? "0" . $g : $g;
-        $b = (strlen("" . $b) === 1) ? "0" . $b : $b;
-
-        return $r . $g . $b;
-    }
-
-    public function darken(int $amount = 10): string
-    {
-        // Darken
-        $darkerHSL = $this->darkenHsl($this->_hsl, $amount);
-        // Return as HEX
-        return self::hslToHex($darkerHSL);
-    }
-
-    private function darkenHsl(array $hsl, int $amount = 10): array
-    {
-        // Check if we were provided a number
-        if ($amount) {
-            $hsl['L'] = ($hsl['L'] * 100) - $amount;
-            $hsl['L'] = ($hsl['L'] < 0) ? 0 : $hsl['L'] / 100;
-        } else {
-            // We need to find out how much to darken
-            $hsl['L'] /= 2;
-        }
-
-        return $hsl;
-    }
-
-    public function isDark($color = false, int $darkerThan = 130): bool
-    {
-        // Get our color
-        $color = ($color) ? $color : $this->_hex;
-
-        // Calculate straight from rbg
-        $r = hexdec($color[0] . $color[1]);
-        $g = hexdec($color[2] . $color[3]);
-        $b = hexdec($color[4] . $color[5]);
-
-        return (($r * 299 + $g * 587 + $b * 114) / 1000 <= $darkerThan);
-    }
-
-    public function lighten(int $amount = 10): string
-    {
-        // Lighten
-        $lighterHSL = $this->lightenHsl($this->_hsl, $amount);
-        // Return as HEX
-        return self::hslToHex($lighterHSL);
-    }
-
-    private function lightenHsl(array $hsl, int $amount = 10): array
-    {
-        // Check if we were provided a number
-        if ($amount) {
-            $hsl['L'] = ($hsl['L'] * 100) + $amount;
-            $hsl['L'] = ($hsl['L'] > 100) ? 1 : $hsl['L'] / 100;
-        } else {
-            // We need to find out how much to lighten
-            $hsl['L'] += (1 - $hsl['L']) / 2;
-        }
-
-        return $hsl;
-    }
-
-    public function isLight($color = false, int $lighterThan = 130): bool
-    {
-        // Get our color
-        $color = ($color) ? $color : $this->_hex;
-
-        // Calculate straight from rbg
-        $r = hexdec($color[0] . $color[1]);
-        $g = hexdec($color[2] . $color[3]);
-        $b = hexdec($color[4] . $color[5]);
-
-        return (($r * 299 + $g * 587 + $b * 114) / 1000 > $lighterThan);
+        return false;
     }
 
 
-    public function mix(string $hex2, int $amount = 0): string
-    {
-        $rgb2 = self::hexToRgb($hex2);
-        $mixed = $this->mixRgb($this->_rgb, $rgb2, $amount);
-        // Return as HEX
-        return self::rgbToHex($mixed);
-    }
 
 }
