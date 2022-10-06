2ff48c893950ae699512c0044dfc5cd212b42490
nam-co <mm@maksuco.com>
2022-10-05 21:03:37 -0500

    bgBlur

--- src/Traits/Colors.php
+++ src/Traits/Colors.php
@@ -6,19 +6,38 @@ use OzdemirBurak\Iris\Color\Hexa;
 trait Colors {
 
 
+    public function checkHEX($color) {
+        $color = str_replace("#", "", $color);
+        if($color=='transparent') {
+            $color = 'FFFFFF00';
+        }
+        if(ctype_xdigit($color) && strlen($color)>5){
+            return str_starts_with($color, '#')? $color : '#'.$color;
+        }
+        return false;
+    }
+
     public function gradientCSS($firstColor,$secondColor=false,$angle=160,$percentage=20,$opacity=0,$amount=15) {
-        $firstColor = $this->checkHEX($firstColor); 
-        if($firstColor==false){ return false; };
+        if(is_string($firstColor)) {
+            $firstColor = $this->checkHEX($firstColor);
+            if($firstColor==false){ return false; };
+        } else {
+            $firstColor = $firstColor->toHexa(); 
+        }
         if($secondColor==false){
             $secondColor = $this->spinColor($firstColor,15,$amount)->fadeOut($opacity);
         }
         return 'linear-gradient('.$angle.'deg, '.$firstColor.' '.$percentage.'%, '.$secondColor.' 100%)';
     }
 
-    public function gradientCSS2($color,$color2=false,$opacity=0) {
-        $color = $this->checkHEX($color); 
-        if($color==false){ return false; };
-        $secondColor = $this->spinColor($color,30,1)->fadeOut($opacity);
+    public function gradientCSS2($firstColor,$color2=false,$opacity=0) {
+        if(is_string($firstColor)) {
+            $firstColor = $this->checkHEX($firstColor);
+            if($firstColor==false){ return false; };
+        } else {
+            $firstColor = $firstColor->toHexa(); 
+        }
+        $secondColor = $this->spinColor($firstColor,30,1)->fadeOut($opacity);
         if($color2) {
             $temp_radial1 = new Hexa($color2);
             $radial1_dist = 100;
@@ -29,13 +48,13 @@ trait Colors {
         $radial1 = $temp_radial1->spin(180)->fade(50);
 
         //$radial1 = $this->spinColor($color,30,70);
-        $radial2 = $this->spinColor($color,30,30)->lighten(70)->fade(50);
+        $radial2 = $this->spinColor($firstColor,30,30)->lighten(70)->fade(50);
         // $radial1Out = $radial1->fade(0);
         // $radial2Out = $radial2->fade(0);
         return <<<HTML
             radial-gradient(circle at $radial1_dist% 65%, $radial1 0%, transparent 70%), 
             radial-gradient(circle at -40% 30%, $radial2 0%, transparent 80%), 
-            linear-gradient(180deg, $color 0%, $secondColor 99.72%);
+            linear-gradient(180deg, $firstColor 0%, $secondColor 99.72%);
         HTML;
     }
 
@@ -63,17 +82,6 @@ trait Colors {
     public function color($color,$function='fade',$amount=0) {
         $color = $this->newColor($color);
         return $color->$function($amount);
-    }
-
-    public function checkHEX($color) {
-        $color = str_replace("#", "", $color);
-        if($color=='transparent') {
-            $color = 'FFFFFF00';
-        }
-        if(ctype_xdigit($color) && strlen($color)>5){
-            return str_starts_with($color, '#')? $color : '#'.$color;
-        }
-        return false;
     }
     
     public function newColor($color) {
@@ -110,12 +118,20 @@ trait Colors {
         HTML;
     }
 
-    public function bgBlend($color,$opacity,$gradient=false) {
-        $color = str_replace("#", "", $color);
-        if(ctype_xdigit($color) && strlen($color)>5){
-            return str_starts_with($color, '#')? $color : '#'.$color;
-        }
-        return false;
+    //use it with .overlay-var
+    public function bgBlend($imgUrl,$color,$opacity=60,$gradient=false) {
+        //$color = str_replace("#", "", $color);
+        $color = $this->color($color,'fade',$opacity);
+        // if(ctype_xdigit($color) && strlen($color)>5){
+        //     return str_starts_with($color, '#')? $color : '#'.$color;
+        // }
+        // return ($gradient)? $this->gradientCSS($color) : $color;
+        $colorCSS = ($gradient)? $this->gradientCSS($color) : 'linear-gradient('.$color.','.$color.')';
+        ray('bgBlend',$gradient,$opacity,$colorCSS,$color,$this->gradientCSS($color));
+        return <<<HTML
+            $colorCSS, 
+            url('$imgUrl');
+        HTML;
     }
 
 
