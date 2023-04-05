8ddb8a6455b24d9b27c4d126930ae2f9e907c5fd
nam-co <mm@maksuco.com>
2023-04-05 10:45:00 -0500

    up

--- src/Helpers.php
+++ src/Helpers.php
@@ -860,6 +860,8 @@ function domain_check($domain) {
 
 function url_html($url,$section='body') {
 	$html = @file_get_contents($url);
+	if(empty($html)){ return ""; }
+	
 	$dom = new \DOMDocument();
 	libxml_use_internal_errors(true);
 	$dom->loadHTML($html);
