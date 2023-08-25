99e65197ed2d4291b0c9b1fb344fde28d055e9d3
nam-co <mm@maksuco.com>
2023-08-25 06:20:46 -0500

    up

--- src/Helpers.php
+++ src/Helpers.php
@@ -137,7 +137,7 @@ function replaceTextBetween($text, $start="", $end="", $replace=false) {
 
 function moneyFormat($value,$currency='USD') {
 		
-	$value = (is_string($value))? (float) $value : $value;
+	$value = (is_string($value))? (float) $value : $value ?? 0;
 	$string = sprintf("%.2f", $value);
 	$decimals = (strpos($string,'.00') !== false)? 0 : 2;
 	
